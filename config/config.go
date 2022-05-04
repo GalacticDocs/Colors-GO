96bf6af7886e564474fc7e7e0d70c45b3eda17e0
Vitaliya <42318815+iVitaliya@users.noreply.github.com>
2022-05-04 12:56:44 +0200

    Update config.go

--- config/config.go
+++ config/config.go
@@ -1,1 +1,23 @@
+package config
 
+import (
+	"log"
+	"os"
+
+	_env "github.com/joho/godotenv"
+)
+
+func init() {
+	err := _env.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func get(key string) string {
+	if os.Getenv(key) == "" {
+		log.Fatal("Environment variable " + key + " is not set")
+	}
+
+	return os.Getenv(key)
+}
